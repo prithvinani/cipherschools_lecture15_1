bfc5c6d493ee4576c7b837c3881518ce6a8167bf
Ramineni Prithvi <95245461+prithvinani@users.noreply.github.com>
2023-07-08 04:01:17 +0530

    Add files via upload

---
+++ lecture15_1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // char r,o,h,i,t;
+    // cin>>r>>o>>h>>i>>t;
+    
+    // char name[1000];
+    // cout<<"enter your name: (NOTE: Enter # after the end of your name )";
+    // int i=0;
+    // // for ( i = 0; i < 5; i++)
+    // // {
+    // //     cin>>name[i];
+    // // }
+    // while (1)
+    // {
+    //     cin>>name[i];
+    //     if (name[i]=='#') break;
+    //     i++;
+    // }
+    // i=0;
+    // cout<<"the name you have just entered is "<<endl;
+    // // for ( i = 0; i < 5; i++)
+    // // {
+    // //     cout<<name[i];
+    // // }
+    // while (name[i]!='#')
+    // {
+    //     cout<<name[i];
+    //     i++;
+    // }
+    string name;
+    cin>>name;
+    cout<<name;
+}
